7b3fca292a3b24ca9885928d4ec4cd507f99708d
Jenny Williamson <jenny@rockmelt.com>
2013-11-24 19:22:09 -0800

    read nonstandard gifs
    These show up fairly frequently in the wild; ImageMagick handles them
    correctly while ImageIO throws an error and JAI and GifDecoder only load
    the top portion of the frames. Ex:
    http://24.media.tumblr.com/tumblr_mceuhx0qgj1r5hubco1_500.gif
    http://uberhumor.com/wp-content/uploads/2012/09/Gyvdz2.gif
    https://i.chzbgr.com/completestore/12/10/23/H0I7k2e8M069BDdrEDb5ww2.gif

---
+++ src/test/java/com/madgag/gif/fmsware/TestGifDecoder.java
@@ -0,0 +1,25 @@
+package com.madgag.gif.fmsware;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import org.junit.Test;
+import sun.misc.IOUtils;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+public class TestGifDecoder {
+
+    @Test
+    public void testWeirdGif() throws Exception {
+        GifDecoder decoder = new GifDecoder();
+        decoder.read(getClass().getResourceAsStream("/brucelee.gif"));
+        BufferedImage image = decoder.getImage();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "gif", outputStream);
+        byte[] actualBytes = outputStream.toByteArray();
+        byte[] expectedBytes = IOUtils.readFully(getClass().getResourceAsStream("/brucelee-frame.gif"), -1, true);
+        assertThat(actualBytes).isEqualTo(expectedBytes);
+    }
+}
